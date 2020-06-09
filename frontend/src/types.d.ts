73ed40d37d8de28aeec8b553a422af511d3e30bb
KauaLimaMartins <kaualimadev0601@gmail.com>
2020-06-09 12:02:29 -0300

    interfaces

---
+++ frontend/src/types.d.ts
@@ -0,0 +1,14 @@
+interface Item {
+    id: number;
+    title: string;
+    image_url: string;
+}
+
+interface IBGEUFResponse {
+    id: number;
+    sigla: string;
+}
+
+interface IBGECityResponse {
+    nome: string;
+}
