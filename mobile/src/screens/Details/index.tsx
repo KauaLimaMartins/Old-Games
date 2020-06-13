586302dd0141d1cfba126f8ced58ab711ae4da7d
KauaLimaMartins <kaualimadev0601@gmail.com>
2020-06-13 19:15:58 -0300

    Details screen

--- mobile/src/screens/Details/index.tsx
+++ mobile/src/screens/Details/index.tsx
@@ -1,10 +1,116 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Feather, FontAwesome } from '@expo/vector-icons';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
+import * as MailComposer from 'expo-mail-composer';
+import {
+    View,
+    TouchableOpacity,
+    Image,
+    Text,
+    SafeAreaView,
+    ScrollView,
+    ActivityIndicator,
+    Linking,
+} from 'react-native';
 
+import api from '../../services/api';
 import styles from './styles';
 
 const Details: React.FC = () => {
-    return <View />;
+    const navigation = useNavigation();
+    const route = useRoute();
+
+    const routeParams = route.params as DetailParams;
+
+    // States
+    const [data, setData] = useState<Data>({} as Data);
+
+    // Effects
+    useEffect(() => {
+        api.get(`games/${routeParams.game_id}`).then((response) =>
+            setData(response.data)
+        );
+    }, []);
+
+    // Functions
+    function handleNavigateBack() {
+        navigation.goBack();
+    }
+
+    function handleComposeMail() {
+        MailComposer.composeAsync({
+            subject: `Interesse no jogo ${data.game.game_name}`,
+            recipients: [data.game.email],
+            body: `Olá ${data.game.owner}, vi que você disponibilizou o game ${data.game.game_name} para doação no site da OldGames, e ...`,
+        });
+    }
+
+    function handleSendWhatsapp() {
+        Linking.openURL(
+            `whatsapp://send?phone=55+${data.game.whatsapp}&text=Tenho interesse no jogo ${data.game.game_name}`
+        );
+    }
+
+    // Shows a loading indicator if data has not yet been received
+    if (!data.game) {
+        return (
+            <ActivityIndicator
+                size="large"
+                color="#AC38FF"
+                style={{ flex: 1 }}
+            />
+        );
+    }
+
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <View style={styles.container}>
+                <TouchableOpacity>
+                    <Feather
+                        name="arrow-left"
+                        size={20}
+                        color="#AC38FF"
+                        onPress={handleNavigateBack}
+                    />
+                </TouchableOpacity>
+                <Image
+                    style={styles.gameImage}
+                    source={{
+                        uri:
+                            'http://lojasaraiva.vteximg.com.br/arquivos/ids/655497/1005800289.jpg?v=636968164320630000',
+                    }}
+                />
+                <ScrollView>
+                    <Text style={styles.gameName}>{data.game.game_name}</Text>
+                    <Text style={styles.gameConsole}>
+                        {data.consoleTitle.map((item) => item.title)}
+                    </Text>
+                    <Text style={styles.gameDescription}>
+                        {data.game.game_description}
+                    </Text>
+                    <Text style={styles.gameOwner}>{data.game.owner}</Text>
+
+                    <View style={styles.address}>
+                        <Text style={styles.addressTitle}>Endereço</Text>
+                        <Text style={styles.addressContent}>
+                            {data.game.city}, {data.game.uf}
+                        </Text>
+                    </View>
+                </ScrollView>
+            </View>
+            <View style={styles.footer}>
+                <RectButton style={styles.button} onPress={handleSendWhatsapp}>
+                    <FontAwesome name="whatsapp" size={20} color="#FFF" />
+                    <Text style={styles.buttonText}>Whatsapp</Text>
+                </RectButton>
+                <RectButton style={styles.button} onPress={handleComposeMail}>
+                    <Feather name="mail" size={20} color="#FFF" />
+                    <Text style={styles.buttonText}>E-mail</Text>
+                </RectButton>
+            </View>
+        </SafeAreaView>
+    );
 };
 
 export default Details;
--- mobile/src/screens/Details/styles.ts
+++ mobile/src/screens/Details/styles.ts
@@ -1,30 +1,46 @@
-import { StyleSheet, Dimensions } from 'react-native';
-
-const { width: Width, height: Height } = Dimensions.get('window');
+import { StyleSheet, Platform } from 'react-native';
+import Constants from 'expo-constants';
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 32,
-        paddingTop: 20,
+        paddingTop: Platform.OS === 'ios' ? 20 : Constants.statusBarHeight + 20,
     },
 
-    pointImage: {
+    gameImage: {
         width: '100%',
-        height: 120,
+        height: '45%',
         resizeMode: 'cover',
         borderRadius: 10,
-        marginTop: 32,
+        marginTop: 28,
+        marginBottom: 20,
     },
 
-    pointName: {
+    gameName: {
         color: '#322153',
         fontSize: 28,
         fontFamily: 'Ubuntu_700Bold',
-        marginTop: 24,
     },
 
-    pointItems: {
+    gameConsole: {
+        fontFamily: 'Roboto_400Regular',
+        fontSize: 22,
+        lineHeight: 24,
+        marginTop: 8,
+        marginBottom: 10,
+        color: '#AC38CC',
+    },
+
+    gameOwner: {
+        fontFamily: 'Roboto_400Regular',
+        lineHeight: 24,
+        fontSize: 18,
+        marginTop: 4,
+        color: '#AC38CC',
+    },
+
+    gameDescription: {
         fontFamily: 'Roboto_400Regular',
         fontSize: 16,
         lineHeight: 24,
@@ -33,7 +49,7 @@ const styles = StyleSheet.create({
     },
 
     address: {
-        marginTop: 32,
+        marginTop: 28,
     },
 
     addressTitle: {
@@ -52,6 +68,7 @@ const styles = StyleSheet.create({
     footer: {
         borderTopWidth: StyleSheet.hairlineWidth,
         borderColor: '#999',
+        marginTop: -20,
         paddingVertical: 20,
         paddingHorizontal: 32,
         flexDirection: 'row',
@@ -60,7 +77,7 @@ const styles = StyleSheet.create({
 
     button: {
         width: '48%',
-        backgroundColor: '#34CB79',
+        backgroundColor: '#AC38FF',
         borderRadius: 10,
         height: 50,
         flexDirection: 'row',
@@ -71,7 +88,7 @@ const styles = StyleSheet.create({
     buttonText: {
         marginLeft: 8,
         color: '#FFF',
-        fontSize: 16,
+        fontSize: 18,
         fontFamily: 'Roboto_500Medium',
     },
 });
