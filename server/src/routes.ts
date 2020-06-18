9fd332b087edebfdeb5fdaa529534603d4ea243a
KauaLimaMartins <kaualimadev0601@gmail.com>
2020-06-18 12:12:44 -0300

    route users

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,14 +1,29 @@
 import { Router } from 'express';
+import multer from 'multer';
 
-import ConsoleController from './controllers/ConsoleController';
-import GameController from './controllers/GameController';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import ConsoleController from './app/controllers/ConsoleController';
+import GameController from './app/controllers/GameController';
+
+import authMiddleware from './app/middlewares/auth';
+
+import multerConfig from './config/multer';
 
 const routes = Router();
+const upload = multer(multerConfig);
+
+routes.post('/users', UserController.store);
+
+routes.post('/sessions', SessionController.store);
 
 routes.get('/consoles', ConsoleController.index);
 
 routes.get('/games/:id', GameController.show);
 routes.get('/games', GameController.index);
-routes.post('/games', GameController.store);
+
+routes.use(authMiddleware);
+
+routes.post('/games', upload.single('image'), GameController.store);
 
 export default routes;
