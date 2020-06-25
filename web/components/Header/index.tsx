369ceaae5ecdf01c742c5fec644f158bfc58b3c3
KauaLimaMartins <kaualimadev0601@gmail.com>
2020-06-25 12:19:33 -0300

    components

---
+++ web/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Header: React.FC = () => {
+    return (
+        <Container>
+            <img src="/logo.png" alt="old games logo" />
+        </Container>
+    );
+};
+
+export default Header;
