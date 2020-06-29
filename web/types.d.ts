c567e15f368cd145d7b3a95f699b0d97cba8ee8a
KauaLimaMartins <kaualimadev0601@gmail.com>
2020-06-29 14:27:40 -0300

    types

--- web/types.d.ts
+++ web/types.d.ts
@@ -5,12 +5,3 @@ interface Item {
     title: string;
     image_url: string;
 }
-
-interface IBGEUFResponse {
-    id: number;
-    sigla: string;
-}
-
-interface IBGECityResponse {
-    nome: string;
-}
