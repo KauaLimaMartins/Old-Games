52dc48d64172083de81ced0cb458ffcdee2c5712
KauaLimaMartins <kaualimadev0601@gmail.com>
2020-07-09 13:49:12 -0300

    some changes

--- web/pages/edit-game.tsx
+++ web/pages/edit-game.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
 import { FaTrash } from 'react-icons/fa';
-import { DefaultSeo } from 'next-seo';
-
-import GlobalStyles from '../styles/global';
-import SEO from '../next-seo.config';
 
 import {
     Container,
@@ -21,54 +17,46 @@ import {
 
 const EditGame: React.FC = () => {
     return (
-        <>
-            <DefaultSeo {...SEO} />
-            <GlobalStyles />
+        <Container>
+            <CenterContainer>
+                <CenterLeftContainer>
+                    <FaTrash size={18} />
 
-            <Container>
-                <CenterContainer>
-                    <CenterLeftContainer>
-                        <FaTrash size={18} />
+                    <Image src="https://assets.b9.com.br/wp-content/uploads/2019/09/Minecraft_Box_Shot_.0-1200x720.jpg" />
 
-                        <Image src="https://assets.b9.com.br/wp-content/uploads/2019/09/Minecraft_Box_Shot_.0-1200x720.jpg" />
-
-                        <Input
-                            placeholder="Novo nome do jogo"
-                            value="Minecraft"
-                        />
-                        <Input
-                            placeholder="Nova descrição do jogo"
-                            value="Minecraft é um jogo muito top mesmo, não tem nem
+                    <Input placeholder="Novo nome do jogo" value="Minecraft" />
+                    <Input
+                        placeholder="Nova descrição do jogo"
+                        value="Minecraft é um jogo muito top mesmo, não tem nem
                                 comparação com outros jogos."
-                        />
-                    </CenterLeftContainer>
-                    <CenterRightContainer>
-                        <TitleRight>
-                            Gostou do novo
-                            <br />
-                            visual do seu
-                            <br />
-                            game?
-                        </TitleRight>
-                        <DescriptionRight>
-                            Terminou de fazer as mudanças do
-                            <br />
-                            seu game? Então finalize!
-                        </DescriptionRight>
+                    />
+                </CenterLeftContainer>
+                <CenterRightContainer>
+                    <TitleRight>
+                        Gostou do novo
+                        <br />
+                        visual do seu
+                        <br />
+                        game?
+                    </TitleRight>
+                    <DescriptionRight>
+                        Terminou de fazer as mudanças do
+                        <br />
+                        seu game? Então finalize!
+                    </DescriptionRight>
 
+                    <Link href="/dashboard">
+                        <ButtonRight>Finalizar</ButtonRight>
+                    </Link>
+                    <CancelEdition>
+                        ou{' '}
                         <Link href="/dashboard">
-                            <ButtonRight>Finalizar</ButtonRight>
+                            <a>cancele as mudanças</a>
                         </Link>
-                        <CancelEdition>
-                            ou{' '}
-                            <Link href="/dashboard">
-                                <a>cancele as mudanças</a>
-                            </Link>
-                        </CancelEdition>
-                    </CenterRightContainer>
-                </CenterContainer>
-            </Container>
-        </>
+                    </CancelEdition>
+                </CenterRightContainer>
+            </CenterContainer>
+        </Container>
     );
 };
 
